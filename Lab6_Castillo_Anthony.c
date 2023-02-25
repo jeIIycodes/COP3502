4e04fad0763dc7af3bee747865319096841e9314
Anthony Castillo <98064363+jeIIycodes@users.noreply.github.com>
2023-02-24 22:55:25 -0500

    Add files via upload

---
+++ Lab6_Castillo_Anthony.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int search(int numbers[], int low, int high, int value) 
+	{
+		if (low > high) 
+		{ 
+			return -1;
+		}
+		
+		int mid = (low + high) / 2;
+		
+		if (numbers[mid] == value) 
+		{ 
+			return mid;
+		} 
+		else if (numbers[mid] > value) 
+		{ 
+			return search(numbers, low, mid - 1, value);
+		} 
+		else 
+		{ 
+			return search(numbers, mid + 1, high, value);
+		}
+	}
+    return 0;
+}
